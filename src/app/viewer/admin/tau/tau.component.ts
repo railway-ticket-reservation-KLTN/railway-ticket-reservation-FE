822352d55a4b527405388340eff6580e8a309515
Nguyen Ba Loc <nguyenlocktpm15a@gmail.com>
2023-05-17 08:03:16 +0700

    update admin

--- src/app/viewer/admin/tau/tau.component.ts
+++ src/app/viewer/admin/tau/tau.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { OtherAdminService } from 'src/app/service/other-admin-service';
 
 @Component({
   selector: 'app-tau',
   templateUrl: './tau.component.html',
   styleUrls: ['./tau.component.css']
 })
-export class TauComponent {
-
+export class TauComponent implements OnInit {
+  HanhTrinh:any[];
+  constructor(
+    private _dialog: MatDialog,
+    private service:OtherAdminService,
+  ) {}
+  ngOnInit() {
+    this.service.getDanhSachHanhTrinh().subscribe(data =>{
+      console.log(data);
+      this.HanhTrinh=data;
+      
+    })
+  }
 }
