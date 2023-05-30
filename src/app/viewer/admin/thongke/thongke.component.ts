a6da36b3dc3529d856d65e4f0e6ac60aee15c6bf
Nguyen Ba Loc <nguyenlocktpm15a@gmail.com>
2023-05-30 17:58:50 +0700

    thong ke complete

--- src/app/viewer/admin/thongke/thongke.component.ts
+++ src/app/viewer/admin/thongke/thongke.component.ts
@@ -11,14 +11,24 @@ import { OtherAdminService } from 'src/app/service/other-admin-service';
 export class ThongkeComponent implements OnInit {
   nam:string ='2023';
   thang:number = 0;
-  years: number[] = [];
+  years: any[] = [];
+  namThongKe :any[]=[];
+  doanhThuResponse:any[] =[];
+  doanhThuThangResponse:any[] =[];
   selectedYear: number;
   chart: Chart;
   soVe:number;
-  doanhThu:any ;
+  doanhThu:any;
+  doanhThuFilter:any;
   namResponse:number;
+  thangResponse:number;
   doanhThuNam : any;
   doanhThuThangNam : any;
+  showResult:boolean;
+  showResult1:boolean;
+
+  thangchart:any[]=[];
+  doanhThuThang:any[] =[];
 
   constructor(
 
@@ -32,24 +42,45 @@ export class ThongkeComponent implements OnInit {
     }
   }
   ngOnInit() {
+    this.showResult = false;
+    this.showResult1 = true;
     this.renderChart();
     this.onNamChange();
+    this.renderChart();
   }
   onNamChange() {
     if (this.nam) {
+      this.showResult = false;
+      this.showResult1 = true;
       this.service.getThongKeTrongNam(this.nam).subscribe(
         (data) => {
          this.doanhThuNam=data;
           console.log(this.doanhThuNam);
-          this.doanhThu = this.doanhThuNam.doanhThu.toLocaleString('vi-VN', { style: 'currency', currency: 'VND' });;
+          this.doanhThu = this.doanhThuNam.doanhThu
+          this.doanhThuFilter = this.doanhThu.toLocaleString('vi-VN', { style: 'currency', currency: 'VND' });
           this.soVe = this.doanhThuNam.soVe;
           this.namResponse = this.doanhThuNam.nam;
-          console.log(this.doanhThu);
+          if (!this.namThongKe.includes(this.namResponse)) {
+            this.namThongKe.push(this.namResponse);
+          }          
+          this.doanhThuResponse.push(this.doanhThu);
+          console.log(this.years);
+          console.log(this.doanhThuResponse);
+          
+          
+          this.chart.destroy();
+          this.renderChart()
           // Xử lý dữ liệu trả về từ AP
         },
         (error) => {
           // Xử lý lỗi khi gọi API
           alert("Không có dữ liệu")
+          this.doanhThuFilter = '';
+          this.soVe =0;
+          this.doanhThuResponse=[];
+          this.chart.destroy();
+          this.renderChart()
+
         }
       );
     }
@@ -57,38 +88,53 @@ export class ThongkeComponent implements OnInit {
 
   onThangNamChange() {
     if (this.nam && this.thang) {
+      this.showResult = true;
+      this.showResult1 = false;
       this.service.getThongKeThangTrongNam(this.nam, this.thang).subscribe(
         (data) => {
-         this.doanhThuThangNam=data;
-          this.doanhThu = this.doanhThuThangNam.doanhThu.toLocaleString('vi-VN', { style: 'currency', currency: 'VND' });;;
+          this.doanhThuThangNam=data;
+          this.doanhThu = this.doanhThuThangNam.doanhThu;
+          this.doanhThuFilter = this.doanhThu.toLocaleString('vi-VN', { style: 'currency', currency: 'VND' });
           this.soVe = this.doanhThuThangNam.soVe;
           this.namResponse = this.doanhThuThangNam.nam;
-          // Xử lý dữ liệu trả về từ AP
+          this.thangResponse = this.doanhThuThangNam.thang;
+          if (!this.thangchart.includes(this.thangResponse)) {
+            this.thangchart.push(this.thangResponse);
+          }  
+          if(this.doanhThuThangNam.thang == this.thang){
+            this.doanhThuResponse.push(this.doanhThu);
+          }    
+          this.chart.destroy();
+          this.renderChart1()
+          this.thangchart =[];
+          this.doanhThuResponse =[];
         },
         (error) => {
           // Xử lý lỗi khi gọi API
           alert("Không có dữ liệu")
+          this.doanhThuFilter = '';
+          this.soVe =0;
+          this.doanhThuResponse=[];
+          this.chart.destroy();
+          this.renderChart1()
         }
       );
     }
   }
   renderChart() {
-
     const canvas = <HTMLCanvasElement>document.getElementById('columnChart');
     const ctx = canvas.getContext('2d');
     if (!ctx) {
       console.error("Không tìm thấy ngữ cảnh vẽ hợp lệ.");
       return;
     }
-  
-
     const data = {
 
-      labels: [1, 2], // Ngày trong tháng
+      labels: this.namThongKe, // Ngày trong tháng
       datasets: [
         {
           label: 'Tổng doanh thu', // Doanh thu
-          data: [100,200], // Dữ liệu doanh thu
+          data: this.doanhThuResponse, // Dữ liệu doanh thu
           backgroundColor: 'rgba(75, 192, 192, 0.5)', // Màu nền cột
           borderColor: 'rgba(75, 192, 192, 1)', // Màu viền cột
           borderWidth: 1
@@ -111,4 +157,41 @@ export class ThongkeComponent implements OnInit {
       options: options
     });
   }
+  renderChart1() {
+    const canvas = <HTMLCanvasElement>document.getElementById('columnChart1');
+    const ctx = canvas.getContext('2d');
+    if (!ctx) {
+      console.error("Không tìm thấy ngữ cảnh vẽ hợp lệ.");
+      return;
+    }
+    const data = {
+
+      labels: this.thangchart, // Ngày trong tháng
+      datasets: [
+        {
+          label: 'Tổng doanh thu', // Doanh thu
+          data: this.doanhThuResponse, // Dữ liệu doanh thu
+          backgroundColor: 'rgba(75, 192, 192, 0.5)', // Màu nền cột
+          borderColor: 'rgba(75, 192, 192, 1)', // Màu viền cột
+          borderWidth: 1
+        }
+      ], 
+    };
+
+// Thêm giá trị doanh thu vào mảng data.datasets[0].data
+    const options: ChartOptions = {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    };
+
+    this.chart = new Chart(ctx, {
+      type: 'bar',
+      data: data,
+      options: options
+    });
+  }
+
 }
